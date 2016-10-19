dc862d447a5166aa22125cd32542fe8c39fb2908
Fabio Teixeira <fabio1079@gmail.com>
2016-10-19 13:05:49 -0200

    Add login form
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- src/tocomfome/Main.java
+++ src/tocomfome/Main.java
@@ -2,32 +2,17 @@
 package tocomfome;
 
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.EventQueue;
+import tocomfome.gui.UserLoginJFrame;
 
-import tocomfome.models.Employee;
-import tocomfome.parsers.EmployeeParser;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            EmployeeParser employeeParser = new EmployeeParser();
-
-            List<Employee> employees = employeeParser.getEmployees();
-
-            Iterator<Employee> employeeIterator = employees.iterator();
-
-            while (employeeIterator.hasNext()) {
-                Employee employee = employeeIterator.next();
-
-                System.out.println("NAME    : " + employee.getName());
-                System.out.println("LOGIN   : " + employee.getLogin());
-                System.out.println("PASSWORD: " + employee.getPassword());
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                final UserLoginJFrame userLoginJFrame = new UserLoginJFrame();
+                userLoginJFrame.setVisible(true);
             }
-        } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        });
     }
 }
